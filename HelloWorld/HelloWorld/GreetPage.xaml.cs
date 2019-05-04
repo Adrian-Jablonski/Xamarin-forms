6a74ef7e6ae901840f61c957044da6a46b69f779
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-04 11:34:04 -0500

    Added label through xaml and programmatically

--- HelloWorld/HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -15,11 +15,22 @@ namespace HelloWorld
 		public GreetPage()
 		{
 			InitializeComponent ();
-		}
 
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            DisplayAlert("Title", "Hello World", "OK");
+
+            Content = new Label
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Text = "Hello World"
+            };  // A way to create user interfaces using code (Same as writing <Label HorizontalOptions="Center" VerticalOptions="Center" Text="Hello World"></Label> in .xaml file)
         }
+
+        // // Button onclick event
+        //private void Button_Clicked(object sender, EventArgs e)
+        //{
+        //    DisplayAlert("Title", "Hello World", "OK");
+        //}
+
+
     }
 }
