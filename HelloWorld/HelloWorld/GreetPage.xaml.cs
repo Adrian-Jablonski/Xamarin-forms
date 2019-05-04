d29d8b7facf2d10f221530534b561fcc0c93088d
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-04 11:51:35 -0500

    Assigned x:Name to slider and label and referenced both fields in codebehind files

--- HelloWorld/HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+// Codebehind file of GreetPage.xaml
 namespace HelloWorld
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
@@ -16,13 +17,14 @@ namespace HelloWorld
 		{
 			InitializeComponent ();
 
+            slider.Value = .5;  // Sets initial slider value to .5
 
-            Content = new Label
-            {
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Center,
-                Text = "Hello World"
-            };  // A way to create user interfaces using code (Same as writing <Label HorizontalOptions="Center" VerticalOptions="Center" Text="Hello World"></Label> in .xaml file)
+            //Content = new Label
+            //{
+            //    HorizontalOptions = LayoutOptions.Center,
+            //    VerticalOptions = LayoutOptions.Center,
+            //    Text = "Hello World"
+            //};  // A way to create user interfaces using code (Same as writing <Label HorizontalOptions="Center" VerticalOptions="Center" Text="Hello World"></Label> in .xaml file)
         }
 
         // // Button onclick event
@@ -31,6 +33,10 @@ namespace HelloWorld
         //    DisplayAlert("Title", "Hello World", "OK");
         //}
 
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            labelId.Text = String.Format("Value is {0:F2}", e.NewValue);    // Sets the text of field with an ID (x:Name) of labelId. F2 sets number to 2 decimal spaces
+        }
 
     }
 }
--- HelloWorld/HelloWorld/obj/Debug/netstandard2.0/GreetPage.xaml.g.cs
+++ HelloWorld/HelloWorld/obj/Debug/netstandard2.0/GreetPage.xaml.g.cs
@@ -17,8 +17,16 @@ namespace HelloWorld {
     public partial class GreetPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Label labelId;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Slider slider;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(GreetPage));
+            labelId = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "labelId");
+            slider = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Slider>(this, "slider");
         }
     }
 }
