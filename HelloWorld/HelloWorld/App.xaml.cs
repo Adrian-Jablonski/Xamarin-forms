00f32eedbe00d492a830fe9ddd5b1fe86ad678ff
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-04 14:59:39 -0500

    Added Grid layout in xaml file

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new StackPage();      // Sets start Page
+            MainPage = new GridPage();      // Sets start Page
         }
 
         protected override void OnStart()
