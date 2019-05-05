06239fa6c6500b49130c870c75d05aed9f58fa5b
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-04 19:54:49 -0500

    Added embeded image to codebehind file

--- HelloWorld/HelloWorld/ImagePage.xaml.cs
+++ HelloWorld/HelloWorld/ImagePage.xaml.cs
@@ -18,12 +18,16 @@ namespace HelloWorld
 
             //var imageSource = (UriImageSource) ImageSource.FromUri(new Uri("http://..."));
 
-            var imageSource = new UriImageSource { Uri = new Uri("http://lorempixel.com/1920/1080/nature/3/") };
-            imageSource.CachingEnabled = false; // Defaults to true when created in xaml
-            // imageSource.CacheValidity = TimeSpan.FromHours(1);  // Defaults cache to 1 day
+            // // **** Loading online image example
+            //var imageSource = new UriImageSource { Uri = new Uri("http://lorempixel.com/1920/1080/nature/3/") };
+            //imageSource.CachingEnabled = false; // Defaults to true when created in xaml
+            // // imageSource.CacheValidity = TimeSpan.FromHours(1);  // Defaults cache to 1 day
 
-            image.Source = imageSource;
+            //image.Source = imageSource;
+            
 
+            // **** Before adding a local image. Need to right click image -> Click properties -> Change build action to embedded resource
+            image.Source = ImageSource.FromResource("HelloWorld.Images.background.PNG");
 
 		}
 	}
