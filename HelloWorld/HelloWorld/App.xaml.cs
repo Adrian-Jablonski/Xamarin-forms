898ec808bd07c4fc5b260601f54bf22f1ba1cef8
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-04 19:38:55 -0500

    Added image page with image and image loader

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new RelativePage();      // Sets start Page
+            MainPage = new ImagePage();      // Sets start Page
         }
 
         protected override void OnStart()
---
+++ HelloWorld/HelloWorld/ImagePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ImagePage : ContentPage
+	{
+		public ImagePage ()
+		{
+			InitializeComponent ();
+
+            //var imageSource = (UriImageSource) ImageSource.FromUri(new Uri("http://..."));
+
+            var imageSource = new UriImageSource { Uri = new Uri("http://lorempixel.com/1920/1080/nature/3/") };
+            imageSource.CachingEnabled = false; // Defaults to true when created in xaml
+            // imageSource.CacheValidity = TimeSpan.FromHours(1);  // Defaults cache to 1 day
+
+            image.Source = imageSource;
+
+
+		}
+	}
+}
