b678d0837b91a862d56d93a6a5c7783af63df06f
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-05 10:08:03 -0500

    Added custom markup extension 'EmbeddedImage' to add a local image directly to xaml code

---
+++ HelloWorld/HelloWorld/MarkupExtensions/EmbeddedImage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+// This is a custom markup extension created to be used inside a .xaml file
+namespace HelloWorld.MarkupExtensions
+{
+    class EmbeddedImage : IMarkupExtension
+    {
+        public string ResourceId { get; set; }  // ResourceId passed in xaml
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (String.IsNullOrWhiteSpace(ResourceId)) {
+                return null;
+            }
+            return ImageSource.FromResource(ResourceId);
+        }
+    }
+}
