a730398120995428eda110b20139a06e2b8e57f2
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-05 16:36:32 -0500

    Added TextCell and ImageCell binding to retreive list data

--- Lists/Lists/BasicList.xaml.cs
+++ Lists/Lists/BasicList.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lists.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,12 +17,19 @@ namespace Lists
 		{
 			InitializeComponent ();
 
-            var names = new List<string>
-            {
-                "Mosh", "John", "Bob"
-            };
+            //var names = new List<string>
+            //{
+            //    "Mosh", "John", "Bob"
+            //};
 
-            listView.ItemsSource = names;
+            //listView.ItemsSource = names;
+
+            listView.ItemsSource = new List<Contact>
+            {
+                new Contact {Name = "Mosh", ImageUrl = "http://lorempixel.com/100/100/people/1"},
+                new Contact {Name = "John", ImageUrl = "http://lorempixel.com/100/100/people/2", Status = "Hey. Let's talk"},
+                new Contact {Name = "Bob", ImageUrl = "http://lorempixel.com/100/100/people/3"},
+            };
 		}
 	}
 }
